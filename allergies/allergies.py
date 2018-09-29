c2a1c1540cf7abde0baaf85fe09b0824702599ea
igorzakhar <igorzakhar@gmail.com>
2018-09-29 17:31:24 +0300

    Add second iteration

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,26 +1,27 @@
 class Allergies(object):
 
-    allergies = [
-        "eggs",
-        "peanuts",
-        "shellfish",
-        "strawberries",
-        "tomatoes",
-        "chocolate",
-        "pollen",
-        "cats"
-    ]
+    allergies = {
+        "eggs": 1,
+        "peanuts": 2,
+        "shellfish": 4,
+        "strawberries": 8,
+        "tomatoes": 16,
+        "chocolate": 32,
+        "pollen": 64,
+        "cats": 128
+    }
 
     def __init__(self, score):
         self.score = score
+        self._lst = [
+            allergie
+            for allergie in self.allergies
+            if self.is_allergic_to(allergie)
+        ]
 
     def is_allergic_to(self, item):
-        return bool(self.score & 1 << self.allergies.index(item))
+        return bool(self.score & self.allergies[item])
 
     @property
     def lst(self):
-        return [
-            self.allergies[i]
-            for i in range(len(self.allergies))
-            if self.is_allergic_to(self.allergies[i])
-        ]
+        return self._lst
--- leap/leap.py
+++
@@ -1,5 +0,0 @@
-def is_leap_year(year):
-    if not(year % 4) and (not(year % 400) or year % 100):
-        return True
-    return False
-
