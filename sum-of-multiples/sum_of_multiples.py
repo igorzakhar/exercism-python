2b2a3db346069d91cac2cdd03b6c6b17e61b2fb5
igorzakhar <igorzakhar@gmail.com>
2018-10-14 13:08:41 +0300

    Remove first guard condition

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,7 +1,4 @@
 def sum_of_multiples(limit, multiples):
-    if not multiples:
-        return 0
-
     return sum([
         num for num in range(limit)
         if any(not num % mul for mul in multiples)
