993d2a6355cc74467826f49437f01d82b658f459
igorzakhar <igorzakhar@gmail.com>
2019-05-25 10:16:33 +0300

    Add the third iteration

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,4 +1,6 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("strand_a and strand_b are not equal")
-    return len([nuc for nuc in zip(strand_a, strand_b) if nuc[0] != nuc[1]])
+    return len(
+        [nucl1 for nucl1, nucl2 in zip(strand_a, strand_b) if nucl1 != nucl2]
+    )
